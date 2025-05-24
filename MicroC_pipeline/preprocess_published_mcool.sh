d5633488542152732c2694da2ccfeae66f0a911d
ukl4001 <ukjinlee@outlook.com>
2025-05-24 13:58:55 -0400

    Fixed preprocessing published mcool files

--- MicroC_pipeline/preprocess_published_mcool.sh
+++ MicroC_pipeline/preprocess_published_mcool.sh
@@ -1,17 +1,42 @@
 conda activate hicexplorer
+COOLDIR="data"
+
 
 EXP="GSE178982_AsyncUT_pooled"
 EXP="GSE178982_AsyncAID_pooled"
 
-hicConvertFormat -m ${COOLDIR}/${EXP}.mcool:://resolutions/1000 --inputFormat cool --outputFormat cool -o ${COOLDIR}/${EXP}_1kb.cool
-hicConvertFormat -m ${COOLDIR}/${EXP}.mcool:://resolutions/5000 --inputFormat cool --outputFormat cool -o ${COOLDIR}/${EXP}_5kb.cool
-hicConvertFormat -m ${COOLDIR}/${EXP}.mcool:://resolutions/10000 --inputFormat cool --outputFormat cool -o ${COOLDIR}/${EXP}_10kb.cool
-hicConvertFormat -m ${COOLDIR}/${EXP}.mcool:://resolutions/100000 --inputFormat cool --outputFormat cool -o ${COOLDIR}/${EXP}_100kb.cool
+# GSE178982 has weight column calculated using ICE normalization
+for RES in 1000 5000 10000 100000; do
+    IN_RES="${COOLDIR}/${EXP}_allRes.mcool::/resolutions/${RES}"
+    KR_OUT="${COOLDIR}/${EXP}_${RES}bp_KR.cool"
 
-cooler coarsen -k 5 -p 32 -o ${COOLDIR}/${EXP}_25kb.cool ${COOLDIR}/${EXP}_5kb.cool
+    # Extract raw .cool at each resolution
+    hicConvertFormat \
+        -m "${IN_RES}" \
+        --inputFormat cool \
+        --outputFormat cool \
+        -o "${KR_OUT}"
+done
 
-hicCorrectMatrix correct -m ${COOLDIR}/${EXP}_1kb.cool --correctionMethod KR --outFileName ${COOLDIR}/${EXP}_1kb_KR.cool
-hicCorrectMatrix correct -m ${COOLDIR}/${EXP}_5kb.cool --correctionMethod KR --outFileName ${COOLDIR}/${EXP}_5kb_KR.cool
-hicCorrectMatrix correct -m ${COOLDIR}/${EXP}_10kb.cool --correctionMethod KR --outFileName ${COOLDIR}/${EXP}_10kb_KR.cool
-hicCorrectMatrix correct -m ${COOLDIR}/${EXP}_25kb.cool --correctionMethod KR --outFileName ${COOLDIR}/${EXP}_25kb_KR.cool
-hicCorrectMatrix correct -m ${COOLDIR}/${EXP}_100kb.cool --correctionMethod KR --outFileName ${COOLDIR}/${EXP}_100kb_KR.cool
+# Make .cool in 25kb resolution by coarsening the 5kb
+COARSEN_IN="${COOLDIR}/${EXP}_5000bp_KR.cool"
+COARSEN_TMP="${COOLDIR}/${EXP}_25000bp_KR.cool"
+cooler coarsen -k 5 -p 32 -o "${COARSEN_TMP}" "${COARSEN_IN}"
+
+# Calculate KR normalization and expected reads.
+for RES in 1000 5000 10000 25000 100000; do
+    KR_OUT="${COOLDIR}/${EXP}_${RES}bp_KR.cool"
+    EXP_OUT="${COOLDIR}/${EXP}_${RES}bp_KR_exp.tsv"
+
+    echo "Balancing (KR) at ${RES}bp..."
+    # recompute KR weights into bins.weight
+    cooler balance --force -p 16 \
+        "${KR_OUT}"
+
+    echo "Computing expected-cis at ${RES}bp..."
+    # compute expected‐cis
+    cooltools expected-cis \
+        -p 16 \
+        -o "${EXP_OUT}" \
+        "${KR_OUT}"
+done
