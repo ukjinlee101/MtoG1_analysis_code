599877091beec87821d1362840ef4c0d9c0d95fe
ukl4001 <ukjinlee@outlook.com>
2025-05-31 17:53:23 -0400

    Chromosight

---
+++ MicroC_loop_chromosight/MicroC_loop_chromosight_detect.sh
@@ -0,0 +1,74 @@
+# FOR SOME REASON, THERE IS MEMORY ISSUE WHEN IT IS RAN USING SBATCH
+# USED INTERACTIVE NODE
+# Interactive node also doesn't work sometime depending on node
+####################################################################################
+conda activate chromosight
+
+sample=("G1DMSO_pooled" "G1dTAG_pooled" "G1A485_pooled")
+resolutions=("25000" "10000" "5000")
+
+coolDir="/athena/apostoloulab/scratch/ukl4001/data/cool_norm_pooled"
+resultDir="/athena/apostoloulab/scratch/ukl4001/data/chromosight"
+mkdir -p $resultDir
+
+for samp in "${sample[@]}"; do
+    for res in "${resolutions[@]}"; do
+        if [[ "$res" == "25000" ]]; then
+            pearson_cutoff=0.3
+            min_separation_value=35000
+            min_dist_value=50000
+            max_dist_value=2000000
+            perc_undetected_value=10
+        elif [[ "$res" == "10000" ]]; then
+            pearson_cutoff=0.3
+            min_separation_value=12000
+            min_dist_value=50000
+            max_dist_value=2000000
+            perc_undetected_value=10
+        elif [[ "$res" == "5000" ]]; then
+            pearson_cutoff=0.3
+            min_separation_value=6000
+            min_dist_value=20000
+            max_dist_value=2000000
+            perc_undetected_value=10
+        else
+            echo "Unknown resolution: $res"
+            continue
+        fi
+        chromosight detect "${coolDir}/${samp}_${res}bp_KR.cool" \
+            "${resultDir}/${samp}_chromosight_${res}bp" \
+            --threads=64 \
+            --pattern=loops \
+            --pearson="${pearson_cutoff}" \
+            --min-separation="${min_separation_value}" \
+            --min-dist="${min_dist_value}" \
+            --max-dist="${max_dist_value}" \
+            --perc-undetected="${perc_undetected_value}"
+    done
+done
+
+
+####################################################################################
+# 240801 quantifying scores
+
+sample=("D3-DMSO_pooled" "D3-dTAG_pooled")
+resolutions=("25" "10")
+
+workDir="/athena/apostoloulab/scratch/ukl4001/Genomics_SMC2_collab/MicroC"
+resultDir="${workDir}/analysis/chromosight/result"
+
+coolDir="${workDir}/result/mcool"
+bedDir="${resultDir}"
+outDir="${resultDir}"
+
+for res in "${resolutions[@]}"; do
+    for samp in "${sample[@]}"; do
+        chromosight quantify ${bedDir}/chromo_cons_${res}kb.bedpe \
+                        ${coolDir}/${samp}_${res}kb_KR.cool \
+                        ${outDir}/${samp}_cons_${res}kb_pu100pz100 \
+                        --pattern=loops \
+                        --threads=10 \
+                        --perc-undetected=100 \
+                        --perc-zero=100
+    done
+done
