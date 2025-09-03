84606e8e2383be6f61aaa55f98609a9943d9c680
UkJin Lee <ukjinlee@outlook.com>
2025-09-03 17:21:53 -0400

    x

--- script/averageBigWig.sh
+++ script/averageBigWig.sh
@@ -16,3 +16,10 @@ wiggletools mean H3K27ac_EpiLC_Rep1_depthNorm.bw H3K27ac_EpiLC_Rep2_depthNorm.bw
 wigToBigWig temp.wig ../../mm10/mm10.chrom.sizes H3K27ac_EpiLC_avg_depthNorm.bw
 rm temp.wig
 
+
+wiggletools mean RAD21_ESC-DMSO_Rep1_spikeNorm_inputNorm.bw RAD21_ESC-DMSO_Rep2_spikeNorm_inputNorm.bw > temp.wig
+wigToBigWig temp.wig ../../mm10/mm10.chrom.sizes RAD21_ESC-DMSO_avg_spikeNorm_inputNorm.bw
+rm temp.wig
+wiggletools mean RAD21_EpiLC-DMSO_Rep1_spikeNorm_inputNorm.bw RAD21_EpiLC-DMSO_Rep2_spikeNorm_inputNorm.bw > temp.wig
+wigToBigWig temp.wig ../../mm10/mm10.chrom.sizes RAD21_EpiLC-DMSO_avg_spikeNorm_inputNorm.bw
+rm temp.wig
